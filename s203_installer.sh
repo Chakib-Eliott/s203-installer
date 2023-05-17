447898e3bece80d1d742770365aec373ef9cbf28
Eliott-B <eliottb.info@gmail.com>
2023-05-17 17:14:12 +0200

    Remove message Add service Add firefox

--- s203_installer.sh
+++ s203_installer.sh
@@ -7,7 +7,7 @@ if [ $? -eq 1 ]
 then
     echo "HTTPD n'est pas installé"
     echo "Installation de HTTPD ..."
-    yum install httpd -y
+    yum install httpd -y > echo
     echo "HTTPD est installé"
 else
     echo "HTTPD est déjà installé"
@@ -20,7 +20,7 @@ if [ $? -eq 1 ]
 then
     echo "PHP n'est pas installé"
     echo "Installation de PHP ..."
-    yum install php -y
+    yum install php -y > echo
     echo "PHP est installé"
 else
     echo "PHP est déjà installé"
@@ -33,18 +33,27 @@ if [ $? -eq 1 ]
 then
     echo "MySQL n'est pas installé"
     echo "Installation de MySQL ..."
-    yum install mysql -y
+    yum install mysql -y > echo
     echo "MySQL est installé"
 else
     echo "MySQL est déjà installé"
 fi
 echo ""
 
+echo "Démarage du service HTTPD ..."
+systemctl start httpd
+echo "Service HTTPD démarré"
+
 echo "Copie du dépôt s203 ..."
-cd /var/www/html
-rm -Rf S203
-git clone https://github.com/Chakib-Eliott/S203.git
+cd /var/www/html > echo
+rm -Rf S203 > echo
+git clone https://github.com/Chakib-Eliott/S203.git > echo
 echo "Copie du dépôt s203 terminée"
 
 echo "Installation terminée !"
+
+echo "Lancement du site ..."
+firefox localhost/S203 & > echo
+echo "Lancement du site terminé !"
+
 exit 0
