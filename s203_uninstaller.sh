741e73c41d3318e1f4b9651fee922bdaf8764fd4
Eliott-B <eliottb.info@gmail.com>
2023-06-05 09:49:43 +0200

    Fix directory error

--- s203_uninstaller.sh
+++ s203_uninstaller.sh
@@ -17,6 +17,7 @@ echo "Suppression du site terminée"
 echo ""
 
 echo "Suppresion de la base de données ..."
+cd $di > /dev/null
 echo 'Entrez S203 pour le mot de passe'
 mysql -p < .conf/delete.sql > /dev/null
 echo "Suppression de la base de données terminée"
@@ -24,7 +25,6 @@ echo "Suppression de la base de données terminée"
 echo ""
 
 echo "Suppression de ce script ..."
-cd $di > /dev/null
 cd .. > /dev/null
 rm -Rf s203-installer > /dev/null
 echo "Suppression du script terminée"
